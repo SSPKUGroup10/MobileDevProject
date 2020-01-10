e865756cb8820d05ceb8792827f52d63c6226fa4
jlxhhh <874589858@qq.com>
2020-01-11 01:40:06 +0800

    fix online getting

--- app/src/main/java/com/example/mobiledevproject/fragment/GroupCheckinFragment.java
+++ app/src/main/java/com/example/mobiledevproject/fragment/GroupCheckinFragment.java
@@ -13,13 +13,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mobiledevproject.MyApp;
 import com.example.mobiledevproject.R;
+import com.example.mobiledevproject.activity.ChangePasswordActivity;
 import com.example.mobiledevproject.adapter.DynamicAdapter;
+import com.example.mobiledevproject.config.StorageConfig;
 import com.example.mobiledevproject.interfaces.GetFragmentInfo;
 import com.example.mobiledevproject.model.Group;
 import com.example.mobiledevproject.model.MessageBean;
 import com.example.mobiledevproject.model.User;
 import com.example.mobiledevproject.util.HttpUtil;
 import com.example.mobiledevproject.util.Utility;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -87,7 +93,7 @@ public class GroupCheckinFragment extends Fragment implements GetFragmentInfo {
 
     private void initDynamic(View view) {
 
-        MyApp app = (MyApp)getActivity().getApplication();
+        MyApp app = (MyApp) getActivity().getApplication();
         User user = app.getUser();
         String groupId = String.valueOf(group.getGroupId());
         String userId = String.valueOf(user.getUserId());
@@ -98,21 +104,25 @@ public class GroupCheckinFragment extends Fragment implements GetFragmentInfo {
         System.out.println(userId);
         try {
             System.out.println();
-            String AbsolutePath= getContext().getFilesDir().toString();
-            File file = new File(AbsolutePath +"/" + userId);
-          //  System.out.println(file.exists());
+            String AbsolutePath = getContext().getFilesDir().toString();
+            File file = new File(AbsolutePath + "/" + userId);
+            //  System.out.println(file.exists());
             if (file.exists()) {
-                List<MessageBean> dynamicData;
-                FileInputStream fileInputStream = getContext().openFileInput(userId);
-                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-                dynamicData = (ArrayList)objectInputStream.readObject();
-                System.out.println("*************************************2");
-                System.out.println(dynamicData.size());
+                List<MessageBean> list = getOnlineContent();
 
-                fileInputStream.close();
-                objectInputStream.close();
+//                List<MessageBean> dynamicData;
+//                FileInputStream fileInputStream = getContext().openFileInput(userId);
+//                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//                dynamicData = (ArrayList) objectInputStream.readObject();
+//                System.out.println("*************************************2");
+//                System.out.println(dynamicData.size());
+//
+//                fileInputStream.close();
+//                objectInputStream.close();
+//
+//                DynamicAdapter dynamicAdapter = new DynamicAdapter(getContext(), dynamicData);
 
-                DynamicAdapter dynamicAdapter = new DynamicAdapter(getContext(), dynamicData);
+                DynamicAdapter dynamicAdapter = new DynamicAdapter(getContext(), list);
                 RecyclerView recyclerView = view.findViewById(R.id.group_message_rv);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                 recyclerView.setAdapter(dynamicAdapter);
@@ -122,8 +132,64 @@ public class GroupCheckinFragment extends Fragment implements GetFragmentInfo {
             e.printStackTrace();
         }
     }
-    public Fragment getCurrentFragment(){
+
+    public Fragment getCurrentFragment() {
         return this;
     }
+
+
+    public List<MessageBean> getOnlineContent() {
+        List<MessageBean> messageBeanList = new ArrayList<>();
+        final boolean[] flag = {false};
+        String token = Utility.getData(getContext(), StorageConfig.SP_KEY_TOKEN);
+
+        MyApp app = (MyApp) getActivity().getApplication();
+        User user = app.getUser();
+        String groupId = String.valueOf(group.getGroupId());
+
+
+        String url = "http://172.81.215.104/api/v1/circles/" + groupId + "/members/" + user.getUserId() + "/clockin/";
+
+
+        HttpUtil.getRequestWithToken(url, token, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseBody = response.body().string();
+                JsonObject jsonObject = new JsonParser().parse(responseBody).getAsJsonObject();
+                JsonArray jsonArray = jsonObject.getAsJsonArray("data");
+
+//                List<MessageBean> messageBeanList = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.size(); i++) {
+                    JsonObject object = jsonArray.get(i).getAsJsonObject();
+                    String userId = String.valueOf(user.getUserId());
+                    String content = object.get("line").getAsString();
+                    List<String> localImages = new ArrayList<>();
+                    List<String> onlineImages = new ArrayList<>();
+                    String picture = object.get("clockinImg").getAsString();
+                    onlineImages.add(picture);
+                    String time = object.get("createdAt").getAsString();
+                    messageBeanList.add(0,new MessageBean(userId,content,localImages,onlineImages,time));
+                }
+
+                System.out.println(jsonArray);
+
+                flag[0] = true;
+            }
+        });
+        while (!flag[0]) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return messageBeanList;
+    }
+
 
 }
